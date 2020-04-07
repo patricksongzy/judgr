e658e6c01286fbd8074f46af5c6ac8e7e50def65
Patrick Song <patricksongzy@gmail.com>
2020-04-07 18:14:28 -0400

    Fix user is confirmed message locale

--- app/controllers/email_confirmations_controller.rb
+++ app/controllers/email_confirmations_controller.rb
@@ -3,6 +3,6 @@ class EmailConfirmationsController < ApplicationController
     user = User.find_by!(email_confirmation_token: params[:token])
     user.confirm_email
     sign_in user
-    redirect_to root_path, notice: I18n.t('users.confirmed')
+    redirect_to root_path, notice: I18n.t('users.confirmed_message')
   end
 end
